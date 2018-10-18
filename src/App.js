6d34ed4486af9833cab5c34a93f26317da53518a
DimaKaplin <dimakaplin.pr@gmail.com>
2018-10-18 17:57:19 +0300

    legend is ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import { LineChart, Line } from 'recharts';
-// import YAxis from 'recharts/lib/cartesian/YAxis';
-// import { XAxis, YAxis } from 'recharts';
 import * as d3 from "d3";
 import axios from 'axios';
 
@@ -48,7 +44,7 @@ class App extends Component {
   
 
   }
-  onePieAnim() {
+  onePieAnim() {                    //one animated pie whithout legend
     const data = [14, 23, 18];
     const w = 400,
           h = 400,
@@ -109,7 +105,7 @@ class App extends Component {
 
   }
 
-  lineGraph() {
+  lineGraph() {                     // lineGrapf whithout animation axis and legend                          
     console.log(this.state.data[1].x)
 
     const dataSortX = this.state.data.map((item)=>{let elem = item.x; return elem});
@@ -138,7 +134,7 @@ class App extends Component {
     svg.attr('height', 500)
       .attr('width', 700);
 
-    // console.log(d3.max(dataSort, (d)=>{return d[0]}))
+ 
 
     svg.append('path').attr('d', line(this.state.data));
 
@@ -146,7 +142,7 @@ class App extends Component {
 
   }
 
-  firstPie() {
+  firstPie() {                      // first and secods pies are parts of twopies
     const w = 400,
           h = 400,
           r = 200,
@@ -200,51 +196,51 @@ class App extends Component {
     .domain([0, 6])
     .range(["grey", "green"]);
 
-const vis = d3.select("svg")
-     //.append("svg")              //create the SVG element inside the <body>
-    .data([this.state.innerData])                   //associate our data with the document
-   // .attr("width", w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
-   // .attr("height", h)
-    .append("g")                //make a group to hold our pie chart
-    .attr("transform", "translate(" + r + "," + r + ")");
+    const vis = d3.select("svg")            
+        .data([this.state.innerData])                   
+        .append("g")                
+        .attr("transform", "translate(" + r + "," + r + ")");
 
-const arc = d3.arc()
-.outerRadius(r)
-.innerRadius(0);
+    const arc = d3.arc()
+        .outerRadius(r)
+        .innerRadius(0);
 
 
 
-const pie = d3.pie()
-     .value((d)=> d.value);
+    const pie = d3.pie()
+        .value((d)=> d.value);
 
-const arcs = vis.selectAll('g.slice')
-    .data(pie)
-    .enter()
-    .append('svg:g')
-    .attr('class', 'slice');
+    const arcs = vis.selectAll('g.slice')
+        .data(pie)
+        .enter()
+        .append('svg:g')
+        .attr('class', 'slice');
 
-arcs.append('svg:path')
-    .attr('fill', (d, i)=> color(i))
-    .attr('d', arc)
-    .attr('class', 'arc')
-    
-arcs.append("svg:text")                                    
-  .attr("transform", function(d) {                   
-    
-    d.innerRadius = 0;
-    d.outerRadius = r;
-    return "translate(" + arc.centroid(d) + ")";        
-})
-.attr("text-anchor", "middle")                         
-.text((d, i) =>this.state.innerData[i].label); 
+    arcs.append('svg:path')
+        .attr('fill', (d, i)=> color(i))
+        .attr('d', arc)
+        .attr('class', 'arc')
+        
+    arcs.append("svg:text")                                    
+      .attr("transform", function(d) {                   
+        
+        d.innerRadius = 0;
+        d.outerRadius = r;
+        return "translate(" + arc.centroid(d) + ")";        
+    })
+    .attr("text-anchor", "middle")                         
+    .text((d, i) =>this.state.innerData[i].label); 
   }
-  twoPies() {
-    console.log(this.state.newData);
+  twoPies() {                       // two animated pies whith legend
+    
+    // functions, vars and scales
+
     let counterTextLeg = 0;
     let counterRectLeg = 0;
-    const svgH = () => document.getElementsByTagName('svg')[0].getBoundingClientRect().height;
+    let counterTextLeg2 = 0;
+    let counterRectLeg2 = 0;
+    const svgH = () => document.getElementsByTagName('svg')[0].getBoundingClientRect().height; // useless functions for adaptiv svg
     const svgW = () => document.getElementsByTagName('svg')[0].getBoundingClientRect().width;
-    const calcRadius = () => {if (h >= w) {return w} else {return h}}; 
     const w = svgW(),
           h = svgH(),
           r = 200,
@@ -260,11 +256,18 @@ arcs.append("svg:text")
             return counterTextLeg },
           calcLegendRect = (x) => {
             counterRectLeg = counterRectLeg + x
-            return counterRectLeg } 
+            return counterRectLeg },
+          calcLegendText2 = (x) => {
+            counterTextLeg2 = counterTextLeg2 + x
+            return counterTextLeg2 },
+          calcLegendRect2 = (x) => {
+            counterRectLeg2 = counterRectLeg2 + x
+            return counterRectLeg2 }  
     console.log(w)
     console.log(h)
     console.log(r2)
 
+// this is d3 code it is compilation from one and two pie
 
 const vis = d3.select("svg")
 
@@ -273,35 +276,34 @@ const vis = d3.select("svg")
     .attr('class', 'all')            
     .attr("transform", "translate(" + 250 + "," + 220 + ")");
 
-    const vis2 = d3.select("svg")
+const vis2 = d3.select("svg")
     .data([this.state.innerData])              
     .append("g")
     .attr('class', 'all')                  
     .attr("transform", "translate(" + 250 + "," + 220 + ")");
+         
+const arc = d3.arc()
+    .outerRadius(r)
+    .innerRadius(r / 2);
+const newArc = d3.arc()
+    .outerRadius(r + 10)
+    .innerRadius(r / 2 + 10)
+    .padAngle(0.02)
 
-    const arc = d3.arc()
-      .outerRadius(r)
-      .innerRadius(r / 2);
-    const newArc = d3.arc()
-      .outerRadius(r + 7)
-      .innerRadius(r / 2 + 7)
-      .padAngle(0.02)
-
-    const arc2 = d3.arc()
-      .outerRadius(r2)
-      .innerRadius(0);
-    const newArc2 = d3.arc()
-      .outerRadius(r2 + 3)
-      .innerRadius(0)
-      .padAngle(0.04);
+const arc2 = d3.arc()
+    .outerRadius(r2)
+    .innerRadius(40);
+const newArc2 = d3.arc()
+    .outerRadius(r2 + 3)
+    .innerRadius(40 + 3)
+    .padAngle(0.05);
 
 
 
 const pie = d3.pie()
-     .value((d)=> d.value)
+    .value((d)=> d.value)
 
-
-const pieAngle = () => {pie.padAngle(0.2); console.log('asdasd')};     
+     
 
 const arcs = vis.selectAll('g.slice')
     .data(pie)
@@ -318,136 +320,204 @@ const arcs2 = vis2.selectAll('g.slice')
 const paths = arcs.append('path')
     .attr('fill', (d, i)=> color(i))
     .attr('d', arc)
+    .style('opacity', 0)
     
 const paths2 = arcs2.append('path')
     .attr('fill', (d, i)=> color2(i))
     .attr('d', arc2)
+    .style('opacity', 0)
     
     
 const text1 = arcs.append("svg:text")
-  .attr('class', 'arc')                                    
-  .attr("transform", function(d) {                   
+    .attr('class', 'arc')                                    
+    .attr("transform", function(d) {                   
     
     d.innerRadius = 0;
     d.outerRadius = r;
-    return "translate(" + arc.centroid(d) + ")";        
-})
-.attr("text-anchor", "middle")                         
-.text((d, i) =>this.state.data[i].label); 
+    return "translate(" + arc.centroid(-50) + ")";        
+    })
+    .attr("text-anchor", "middle")
+    .style('opacity', 0)                         
+    // .text((d, i) =>this.state.data[i].label); 
 
 
 
 const text2 = arcs2.append("svg:text")   
-.attr('class', 'arc')                                 
+  .attr('class', 'arc')                                 
   .attr("transform", function(d) {                   
     
-    d.innerRadius = 0;
-    d.outerRadius = r2;
-    return "translate(" + arc2.centroid(d) + ")";        
-})
-.attr("text-anchor", "middle")                         
-.text((d, i) =>this.state.innerData[i].label);
+  d.innerRadius = 0;
+  d.outerRadius = r2;
+  return "translate(" + arc2.centroid(-50) + ")";        
+  })
+  .attr("text-anchor", "middle")
+  .style('opacity', 0)                         
+  // .text((d, i) =>this.state.innerData[i].label);
+  paths.transition()
+      .duration(3000)
+      .style('opacity', 1)
+  
+  paths2.transition()
+      .duration(3000)
+      .style('opacity', 1)
 
-// func for anim
+
+// funcs for animation
 
 const animPathOver = ()=> {
   paths
-  .transition()
-  .delay(200)
-  .duration(1000)
-.attr('d', newArc)
+    .transition()
+    .delay(200)
+    .duration(1000)
+    .style('opacity', 1)
+    .attr('d', newArc)
 
-text1.transition()
-.delay(200)
-.duration(1000)
-.attr("transform", function(d) {                   
+  text1.transition()
+    .delay(200)
+    .duration(1000)
+    .style('opacity', 1)
+    .attr("transform", function(d) {                   
+        
+    d.innerRadius = 0;
+    d.outerRadius = r;
+    return "translate(" + newArc.centroid(d) + ")";        
+    }).text((d, i) =>d.value+'%')
+    .style('opacity', 1)
+
+    paths2
+    .transition()
+    .delay(200)
+    .duration(1000)
+    .style('opacity', 1)
+    .attr('d', newArc2)
+
+    text2.transition()
+    .delay(200)
+    .duration(1000)
+    .attr("transform", function(d) {                   
+      
+    d.innerRadius = 0;
+    d.outerRadius = r;
+    return "translate(" + newArc2.centroid(d) + ")";        
+    }).text((d, i) =>d.value+'%')
+    .style('opacity', 1)
     
-  d.innerRadius = 0;
-  d.outerRadius = r;
-  return "translate(" + newArc.centroid(d) + ")";        
-}).text((d, i) =>d.value+'%')
-paths2
-.transition()
-.delay(200)
-.duration(1000)
-.attr('d', newArc2)
-
-text2.transition()
-.delay(200)
-.duration(1000)
-.attr("transform", function(d) {                   
-  
-d.innerRadius = 0;
-d.outerRadius = r;
-return "translate(" + newArc2.centroid(d) + ")";        
-}).text((d, i) =>d.value+'%')
 
 
 }
 
 const animPathOut = ()=> {
   paths
-  .transition()
-  .delay(50)
-  .duration(500)
-.attr('d', arc)
+    .transition()
+    .delay(50)
+    .duration(500)
+    .attr('d', arc)
+    .style('opacity', 1)
 
-text1.transition()
-.delay(50)
-.duration(500)
-.attr("transform", function(d) {                   
-    
-  d.innerRadius = 0;
-  d.outerRadius = r;
-  return "translate(" + arc.centroid(d) + ")";        
-}).text((d, i) =>this.state.data[i].label);
-paths2
-.transition()
-.delay(50)
-.duration(500)
-.attr('d', arc2)
+  text1.transition()
+    .delay(50)
+    .duration(500)
+    .attr("transform", function(d) {                   
+      
+    d.innerRadius = 0;
+    d.outerRadius = r;
+    return "translate(" + arc.centroid(-50) + ")";        
+    }).text((d, i) =>this.state.data[i].value+'%')
+    .style('opacity', 0)
 
-text2.transition()
-.delay(50)
-.duration(500)
-.attr("transform", function(d) {                   
+  paths2
+    .transition()
+    .delay(50)
+    .duration(500)
+    .attr('d', arc2)
+    .style('opacity', 1)
+
+  text2.transition()
+    .delay(50)
+    .duration(500)
+    .attr("transform", function(d) {                   
   
-d.innerRadius = 0;
-d.outerRadius = r;
-return "translate(" + arc2.centroid(d) + ")";        
-}).text((d, i) =>this.state.innerData[i].label);
+    d.innerRadius = 0;
+    d.outerRadius = r;
+    return "translate(" + arc2.centroid(-50) + ")";        
+    }).text((d, i) =>this.state.innerData[i].value+'%')
+    .style('opacity', 0)
 
-}
+  }
 
 
 console.log(document.getElementsByTagName('svg')[0].getBoundingClientRect());
 
 
 d3.selectAll('.all')// .transition().style("color","grey").duration(5000)
-.on('mouseover', animPathOver)
-.on('mouseout', animPathOut)
+    .on('mouseover', animPathOver)
+    .on('mouseout', animPathOut)
 
-const legend = d3.select('svg')
-  .append('g')
-  .attr('class', 'legend');
-const elems = legend.selectAll('.legendElem')
-  .data(this.state.data)
-  .enter()
-  .append('text')
-  .text((d, i) =>d.label)
-  .attr('class', 'legendElem')
-  .attr("x", (d) => calcLegendText(100))
-  .attr("y", 500 - 30)
-  
-const rects = legend.selectAll('rect')
+// this is attemt of legend realisation
+
+
+
+const legNew = d3.select('svg')
+    .append('g')
+    .attr('class', 'newLeg')
+
+const newElems = legNew.selectAll('.rects')
     .data(this.state.data)
     .enter()
     .append('rect')
-    .attr('height', 10)
-    .attr('width', 10)
-    .attr("x", (d) => calcLegendRect(130))
-    .attr("y", 500 - 39)
+    .attr('class','rects')
+    .attr('height', 20)
+    .attr('width', 0)
+    .attr('y', 435)
+    .attr('x', (d)=>calcLegendRect(100))
     .attr('fill', (d, i)=> color(i))
+const newText = legNew.selectAll('text')
+    .data(this.state.data)
+    .enter()
+    .append('text')
+    .text((d)=> d.label)
+    .style('fill', 'white')
+    .attr('y', 450)
+    .attr('x', (d)=>calcLegendText(105)+10)
+    .style('opacity', 0)
+    
+    newText.transition()
+    .duration(3000)
+    .style('opacity', 1)
+    newElems.transition()
+    .duration(3000)
+    .attr('width', 100)
+
+    const legNew2 = d3.select('svg')
+    .append('g')
+    .attr('class', 'newLeg2')
+
+const newElems2 = legNew2.selectAll('.rects2')
+    .data(this.state.innerData)
+    .enter()
+    .append('rect')
+    .attr('class','rects2')
+    .attr('height', 20)
+    .attr('width', 0)
+    .attr('y', 470)
+    .attr('x', (d)=>calcLegendRect2(71))
+    .attr('fill', (d, i)=> color2(i))
+const newText2 = legNew2.selectAll('text')
+    .data(this.state.innerData)
+    .enter()
+    .append('text')
+    .text((d)=> d.label)
+    .style('fill', 'white')
+    .attr('y', 485)
+    .attr('x', (d)=>calcLegendText2(75)+10)
+    .style('opacity', 0)
+    
+    newText2.transition()
+    .duration(3000)
+    .style('opacity', 1)
+    newElems2.transition()
+    .duration(3000)
+    .attr('width', 71)
 
   
 
